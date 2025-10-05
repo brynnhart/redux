a2ff63c8e876c3a73f99430f83f5c724929b7413
brynnhart <98272593+brynnhart@users.noreply.github.com>
2025-10-05 13:24:04 -0400

    Add SQLite database setup and seed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 
+require('./db');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const publicDir = path.join(__dirname, 'public');
