07cf59023caea58767507ae12e7d1a27eb99140b
brynnhart <98272593+brynnhart@users.noreply.github.com>
2025-10-06 19:22:39 -0400

    Format conjugal marriage dates

--- views/conjugal.js
+++ views/conjugal.js
@@ -2,7 +2,10 @@ module.exports = {
   async get(ctx) {
     const marriages = ctx.db
       .prepare(
-        `SELECT c1.name AS p1, c2.name AS p2, m.since
+        `SELECT
+           c1.name AS p1,
+           c2.name AS p2,
+           strftime('%Y-%m-%d', m.since) AS since
          FROM marriages m
          JOIN characters c1 ON c1.id = m.char1_id
          JOIN characters c2 ON c2.id = m.char2_id
