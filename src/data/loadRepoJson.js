16db59c5be87a30b14d2cf6832f1e9de2c0f9841
brynnhart <98272593+brynnhart@users.noreply.github.com>
2026-03-06 15:25:26 -0500

    Nuking repo.  Starting over... again.

--- src/data/loadRepoJson.js
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'node:fs';
-import { fileURLToPath } from 'node:url';
-
-export function loadRepoJson(fileName, moduleUrl) {
-    const fileUrl = new URL(`../../data/${fileName}`, moduleUrl);
-    return JSON.parse(readFileSync(fileURLToPath(fileUrl), 'utf-8'));
-}
